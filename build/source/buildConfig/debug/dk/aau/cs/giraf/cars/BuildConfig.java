86998833dfed7ea2e4801fdbe13fa51ba6f02b39
Christian OKeeffe <christian@joedal.dk>
2014-02-18 15:20:33 +0100

    Test of AS

---
+++ build/source/buildConfig/debug/dk/aau/cs/giraf/cars/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dk.aau.cs.giraf.cars;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "dk.aau.cs.giraf.cars";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+}
