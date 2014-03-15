0f4fedbac2e053a7442ad2bd7c79c54ab8057cb7
deaddog <mikkelslsl@gmail.com>
2014-03-16 00:29:40 +0100

    Added a Car to the GameScreen.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/GameScreen.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/GameScreen.java
@@ -1,21 +1,30 @@
 package dk.aau.cs.giraf.cars.game;
 
+import android.graphics.Rect;
+
 import dk.aau.cs.giraf.cars.framework.Game;
 import dk.aau.cs.giraf.cars.framework.Screen;
+import dk.aau.cs.giraf.cars.framework.Graphics;
 
 public class GameScreen extends Screen {
+    private Car car;
+
     public GameScreen(Game game) {
         super(game);
+        this.car = new Car(new Rect(0, 0, 200, 99));
+        this.car.SetPosition(50, (game.getGraphics().getHeight() - car.GetBounds().height()) / 2);
     }
 
     @Override
     public void update(float deltaTime) {
-        
+        car.Update(deltaTime);
     }
 
     @Override
     public void paint(float deltaTime) {
-
+        Graphics graphics = game.getGraphics();
+        graphics.drawARGB(255, 255, 255, 255);
+        car.Paint(graphics, deltaTime);
     }
 
     @Override
