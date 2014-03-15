0e46739f9d0ee943514cf981f49fb83a1b7f5bbe
deaddog <mikkelslsl@gmail.com>
2014-03-15 17:22:50 +0100

    Create all android-specific implementations of the framework as specified by 'http://www.kilobolt.com/day-6-the-android-game-framework-part-ii.html'.

---
+++ cars/src/main/java/dk/aau/cs/giraf/cars/framework/implementation/TouchHandler.java
@@ -0,0 +1,17 @@
+package dk.aau.cs.giraf.cars.framework.implementation;
+
+import java.util.List;
+
+import android.view.View.OnTouchListener;
+
+import dk.aau.cs.giraf.cars.framework.Input.TouchEvent;
+
+public interface TouchHandler extends OnTouchListener {
+    public boolean isTouchDown(int pointer);
+
+    public int getTouchX(int pointer);
+
+    public int getTouchY(int pointer);
+
+    public List<TouchEvent> getTouchEvents();
+}
