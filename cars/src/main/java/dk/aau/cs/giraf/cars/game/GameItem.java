25c3a226628336f69e81b5ddae6c189b361ec3b6
deaddog <mikkelslsl@gmail.com>
2014-03-15 20:38:04 +0100

    Implemented size and location information for GameItem.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/GameItem.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/GameItem.java
@@ -1,8 +1,37 @@
 package dk.aau.cs.giraf.cars.game;
 
+import android.graphics.Rect;
+
 import dk.aau.cs.giraf.cars.framework.implementation.AndroidGraphics;
 
 public abstract class GameItem {
+    private Rect bounds;
+
+    public GameItem(Rect bounds) {
+        this.bounds = bounds;
+    }
+
+    public Rect GetBounds() {
+        return bounds;
+    }
+
+    public void SetPosition(int x, int y) {
+        this.bounds.right += (x - this.bounds.left);
+        this.bounds.bottom += (y - this.bounds.top);
+        this.bounds.left = x;
+        this.bounds.top = y;
+    }
+
+    public void SetSize(int w, int h) {
+        this.bounds.right = this.bounds.left + w;
+        this.bounds.bottom = this.bounds.top + h;
+    }
+
+    public boolean CollidesWith(GameItem item) {
+        return this.bounds.intersect(item.bounds);
+    }
+
     public abstract void Paint(AndroidGraphics graphics, float deltaTime);
+
     public abstract void Update(float deltaTime);
 }
