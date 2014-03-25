bb0ff28a79b3029f935eb9bcb94e7ed33697cd53
Ham den Seje <mr.thalmann@gmail.com>
2014-03-25 14:45:46 +0100

    made buttons fit the size of the screen

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/WinningOverlay.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/WinningOverlay.java
@@ -20,37 +20,29 @@ public class WinningOverlay {
         for (int i = 0; i < len; i++) {
             Input.TouchEvent event = touchEvents.get(i);
             if (event.type == Input.TouchEvent.TOUCH_UP) {
-                if (inBounds(event, 0, 0, 800, 240)) {
-
-                    if (!inBounds(event, 0, 0, 35, 35)) {
+                if (inBounds(event, 0,game.getHeight()/2,game.getWidth()/2, game.getHeight()/2)) {
                         game.setScreen(new GameScreen(game, new TestObstacles()));
                         return GameState.Running;
                     }
-                }
-
                 if (inBounds(event, 0, 240, 800, 240)) {
-                    //nullify();
-                    //goToMenu();
+                    //Go to menu and garbagecollect game
                 }
             }
         }
         return GameState.Won;
     }
 
-    private boolean inBounds(Input.TouchEvent event, int x, int y, int width,
-                             int height) {
-        if (event.x > x && event.x < x + width - 1 && event.y > y
-                && event.y < y + height - 1)
-            return true;
-        else
-            return false;
+    private boolean inBounds(Input.TouchEvent event, int x, int y, int width, int height)
+    {
+        return event.x > x && event.x < x + width - 1 && event.y > y && event.y < y + height - 1;
     }
 
     public void Draw(Game game, Paint paint)
     {
         Graphics g = game.getGraphics();
-        g.drawARGB(155, 0, 0, 0);
-        g.drawString("Spil igen", 400, 165, paint);
-        g.drawString("Menu", 400, 360, paint);
+        g.drawARGB(50, 0, 0, 0);
+        g.drawImage(Assets.GetTrophy(), (int)(game.getWidth()*.50) - Assets.GetTrophy().getWidth()/2, (int)(game.getHeight()*.25) - Assets.GetTrophy().getHeight()/2);
+        g.drawString("Spil igen", (int)(game.getWidth()*.25), (int)(game.getHeight()*.85), paint);
+        g.drawString("Menu", (int)(game.getWidth()*.75), (int)(game.getHeight()*.85), paint);
     }
 }
