6a026b0d29b3e92a31c3c2e78e7500cbd5f7363d
stufkan <stufkan@gmail.com>
2014-03-25 16:02:21 +0100

    Further implementation of the Wedge obstacle placement algorithm

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/WedgeObstacles.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/WedgeObstacles.java
@@ -1,7 +1,11 @@
 package dk.aau.cs.giraf.cars.game;
 
 
+import android.graphics.Point;
 import android.graphics.PointF;
+
+import java.util.Random;
+import java.util.Vector;
 
 public class WedgeObstacles implements ObstacleGenerator{
     @Override
@@ -14,23 +18,62 @@ public class WedgeObstacles implements ObstacleGenerator{
     private float verticalSpeed;
     private float bufferMultipler; // the buffer given to react to the next obstacle
     private int gameHeight;
+    private int gameWidth;
 
-    public WedgeObstacles(float carWidth, float horizontalSpeed,float verticalSpeed,float bufferMultipler, int gameHeight)
+    public WedgeObstacles(float carWidth, float horizontalSpeed,float verticalSpeed,float bufferMultipler, int gameHeight, int gameWidth)
     {
         this.carWidth = carWidth;
         this.horizontalSpeed = horizontalSpeed;
         this.verticalSpeed = verticalSpeed;
         this.bufferMultipler = bufferMultipler;
         this.gameHeight = gameHeight;
+        this.gameWidth = gameWidth;
+    }
+    private PointF placeObstacleAfterWedge(PointF[] wedge )
+    {
+
     }
 
-    private PointF[] calculateWedge(PointF carPosition)
+    private PointF findObstaclePlacement(PointF[] wedge )
+    {
+        Random rand = new Random();
+        PointF left;
+        PointF right;
+
+        Boolean r = rand.nextBoolean();
+        if(r) {
+            left = wedge[0];
+            right = wedge[1];
+        }
+        else {
+            left = wedge[2];
+            right = wedge[3];
+        }
+
+        PointF vec = new PointF(right.x - left.x,right.y-left.y);
+        float length = vec.length();
+
+        float placement = rand.nextFloat()*length;
+
+        PointF displacedPoint = displacePoint(left,right,length/placement);
+    }
+
+    private PointF displacePoint(PointF p, PointF x, float displacementFactor )
+    {
+        return new PointF( (p.x-x.x)*displacementFactor + x.x,(p.y-x.y)*displacementFactor + x.y);
+    }
+
+    private PointF[] calculateWedge(PointF carPosition, float carHeight)
     {
         float angle = wedgeAngle(horizontalSpeed,verticalSpeed);
-        PointF up = findIntersection(carPosition,angle,gameHeight);
-        PointF down = findIntersection(carPosition,-angle,0);
 
-        PointF[] points={carPosition, up, down};
+        PointF topWedgeCarCoord = new PointF(carPosition.x, carPosition.y - carHeight/2);
+        PointF bottomWedgeCarCoord = new PointF(carPosition.x, carPosition.y + carHeight/2);
+
+        PointF bottomWedgeIntersectionCoord = findIntersection(topWedgeCarCoord,angle,gameHeight);
+        PointF topWedgeIntersectionCoord = findIntersection(bottomWedgeCarCoord,-angle,0);
+
+        PointF[] points={topWedgeCarCoord,topWedgeIntersectionCoord,bottomWedgeCarCoord,bottomWedgeIntersectionCoord};
 
         return points;
     }
@@ -45,12 +88,12 @@ public class WedgeObstacles implements ObstacleGenerator{
 
     private float wedgeAngle(float horizontal, float vertical)
     {
-        float moveVector = moveVectorLength(horizontal,vertical);
+        float moveVector = pythagorean(horizontal, vertical);
             return (float)Math.acos(Math.pow(horizontal,2) + Math.pow(moveVector,2) - Math.pow(vertical,2))
                     / (2*horizontal *moveVector);
     }
 
-    private float moveVectorLength(float horizontal, float vertical)
+    private float pythagorean(float horizontal, float vertical)
     {
             return (float)Math.sqrt(Math.pow( horizontal,2) + Math.pow(vertical,2));
     }
