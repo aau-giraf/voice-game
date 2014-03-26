ef1184dedff54dcbe775fff2bd0f841e745afb4b
Ham den Seje <mr.thalmann@gmail.com>
2014-03-26 13:09:37 +0100

    added class for crash overlay

---
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/CrashOverlay.java
@@ -0,0 +1,12 @@
+package dk.aau.cs.giraf.cars.game;
+
+import android.graphics.Paint;
+
+import dk.aau.cs.giraf.cars.framework.Game;
+
+public class CrashOverlay implements Overlay {
+    public void Draw(Game game, Paint paint)
+    {
+        
+    }
+}
