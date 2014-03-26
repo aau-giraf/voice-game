3bfe47035f35dde9f9da64979824bacc2c7d7e4d
stufkan <stufkan@gmail.com>
2014-03-26 16:01:18 +0100

    Layout and logic in settings

--- cars/src/main/java/dk/aau/cs/giraf/cars/Settings.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/Settings.java
@@ -4,13 +4,37 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import dk.aau.cs.giraf.cars.game.GameSettings;
+
+import java.util.LinkedList;
 
 
 public class Settings extends Activity {
 
+    Spinner spinner1;
+    Spinner spinner2;
+    Spinner spinner3;
+    TextView speed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        spinner1 = (Spinner)findViewById(R.id.spinner);
+        spinner2= (Spinner)findViewById(R.id.spinner2);
+        spinner3 = (Spinner)findViewById(R.id.spinner3);
+        speed = (TextView)findViewById(R.id.speed);
+
+        ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(this,R.array.colors_array,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
+        spinner3.setAdapter(adapter);
+
         setContentView(R.layout.activity_settings);
     }
 
@@ -33,6 +57,18 @@ public class Settings extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void OkButtonClick(View view)
+    {
+        LinkedList<Integer> colors = new LinkedList<Integer>();
+        colors.add((Integer)spinner1.getSelectedItem());
+        colors.add((Integer)spinner2.getSelectedItem());
+        colors.add((Integer)spinner3.getSelectedItem());
+
+        GameSettings gs = new GameSettings(colors, Integer.parseInt(speed.getText().toString()));
+
+
     }
 
 }
