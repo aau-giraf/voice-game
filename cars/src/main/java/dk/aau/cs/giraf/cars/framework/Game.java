00c6202cf759496aabe84807af5cecefb29d89a2
deaddog <mikkelslsl@gmail.com>
2014-03-27 13:04:00 +0100

    Added methods from GameActivity that should be shared by both game-types.

--- cars/src/main/java/dk/aau/cs/giraf/cars/framework/Game.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/framework/Game.java
@@ -1,4 +1,17 @@
 package dk.aau.cs.giraf.cars.framework;
 
 public interface Game {
+    public Input getInput();
+
+    public FileIO getFileIO();
+
+    public Graphics getGraphics();
+
+    public Audio getAudio() ;
+
+    public void setScreen(Screen screen);
+
+    public Screen getCurrentScreen();
+
+    public abstract Screen getInitScreen();
 }
