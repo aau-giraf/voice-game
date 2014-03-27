d3f193c3ef1afca7f858780bbe6181c71a0e928a
deaddog <mikkelslsl@gmail.com>
2014-03-27 13:26:53 +0100

    Added the fields from GameActivity to GameFragment.

--- cars/src/main/java/dk/aau/cs/giraf/cars/framework/GameFragment.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/framework/GameFragment.java
@@ -1,6 +1,16 @@
 package dk.aau.cs.giraf.cars.framework;
 
 import android.app.Fragment;
+import android.graphics.Point;
+import android.os.PowerManager;
 
 public abstract class GameFragment extends Fragment implements Game {
+    FastRenderView renderView;
+    Graphics graphics;
+    Audio audio;
+    Input input;
+    FileIO fileIO;
+    Screen screen;
+    PowerManager.WakeLock wakeLock;
+    Point size;
 }
