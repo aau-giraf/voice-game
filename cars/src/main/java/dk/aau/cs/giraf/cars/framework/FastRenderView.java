6511c4baf49f5ffd78438f9a8acb20b2c3499c08
deaddog <mikkelslsl@gmail.com>
2014-04-03 09:06:44 +0200

    FastRenderView sets startTime on first draw.

--- cars/src/main/java/dk/aau/cs/giraf/cars/framework/FastRenderView.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/framework/FastRenderView.java
@@ -30,6 +30,8 @@ public class FastRenderView extends SurfaceView implements Runnable {
 
     }
 
+    private boolean firstdraw = true;
+
     public void run() {
         Rect dstRect = new Rect();
         long startTime = System.nanoTime();
@@ -37,6 +39,10 @@ public class FastRenderView extends SurfaceView implements Runnable {
             if (!holder.getSurface().isValid())
                 continue;
 
+            if (firstdraw) {
+                startTime = System.nanoTime();
+                firstdraw = false;
+            }
 
             // Calculate deltaTime as milliseconds
             float deltaTime = (System.nanoTime() - startTime) / 1000000.000f;
