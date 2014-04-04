7ece09d285a53f822b2a7c3726df7905bc066363
deaddog <mikkelslsl@gmail.com>
2014-04-04 13:36:24 +0200

    CrashOverlay represents the last crash point as a rectangle - simplifying the execution of the Draw method.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/CrashOverlay.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/CrashOverlay.java
@@ -1,30 +1,30 @@
 package dk.aau.cs.giraf.cars.game.Overlay;
 
 import android.graphics.Point;
+import android.graphics.Rect;
 
 import dk.aau.cs.giraf.cars.R;
 import dk.aau.cs.giraf.cars.framework.Game;
 import dk.aau.cs.giraf.cars.framework.Graphics;
 import dk.aau.cs.giraf.cars.framework.Input;
 import dk.aau.cs.giraf.cars.game.Assets;
-import dk.aau.cs.giraf.cars.game.Overlay.Overlay;
-import dk.aau.cs.giraf.cars.game.Overlay.OverlayButton;
 
 public class CrashOverlay extends Overlay {
     private OverlayButton continueButton;
-    Point lastCrash = null;
-    int explosionWidth = 100;
-    int explosionHeight = 100;
+    private Rect lastCrash;
+    private Rect explosionRect;
 
-    public void setLastCrash(Point p)
-    {
-        lastCrash = p;
+    public void setLastCrash(Point p) {
+        lastCrash.offsetTo(p.x, p.y);
+        lastCrash.offset(-lastCrash.width() / 2, -lastCrash.height() / 2);
     }
 
-    public CrashOverlay(Game game){
-        super(game);
-        continueButton = new OverlayButton(game, game.getWidth() / 2, game.getHeight()/2,game.getResources().getString(R.string.crash_button_text));
+    public CrashOverlay(Game game) {
+        continueButton = new OverlayButton(game, game.getWidth() / 2, game.getHeight() / 2, game.getResources().getString(R.string.crash_button_text));
         super.Add(continueButton);
+
+        this.lastCrash = new Rect(0, 0, 100, 100);
+        this.explosionRect = new Rect(0, 0, Assets.GetExplosion().getWidth(), Assets.GetExplosion().getHeight());
     }
 
     public boolean ContinueButtonPressed(Input.TouchEvent[] touchEvents) {
@@ -32,10 +32,9 @@ public class CrashOverlay extends Overlay {
     }
 
     @Override
-    public void Draw(Graphics graphics, float deltaTime)
-    {
-        graphics.drawScaledImage(Assets.GetExplosion(), lastCrash.x - explosionWidth/2, lastCrash.y - explosionHeight/2, explosionWidth, explosionHeight, 0, 0, Assets.GetExplosion().getWidth(), Assets.GetExplosion().getHeight());
-        graphics.drawARGB(155,0,0,0);
-        super.Draw(graphics,deltaTime);
+    public void Draw(Graphics graphics, float deltaTime) {
+        graphics.drawScaledImage(Assets.GetExplosion(), lastCrash, explosionRect);
+        graphics.drawARGB(155, 0, 0, 0);
+        super.Draw(graphics, deltaTime);
     }
 }
