c2129ef8e15ed048b13200f2dd4f6aa3af39c8ec
stufkan <stufkan@gmail.com>
2014-04-04 14:58:41 +0200

    created 3 test maps

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/TestObstacles.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/TestObstacles.java
@@ -1,11 +1,28 @@
 package dk.aau.cs.giraf.cars.game;
 
 public class TestObstacles implements ObstacleGenerator {
+    private final int TESTSCENARIO = 2;
+
     @Override
     public Obstacle[] CreateObstacles(int width, int height) {
-        return new Obstacle[]{
-                //new Obstacle(100, 100, 100, 100),
-                //new Obstacle(500, 300, 100, 100)
-        };
+
+        switch (TESTSCENARIO) {
+            case 0:// top + mid
+                return new Obstacle[]{
+                        new Obstacle((float) 0.2 * width, (float) 0.15 * height, 100, 100),
+                        new Obstacle((float) 0.5 * width, (float) 0.45 * height, 100, 100)
+                };
+            case 1: // top + bottom
+                return new Obstacle[]{
+                        new Obstacle((float) 0.5 * width, (float) 0.15 * height, 100, 100),
+                        new Obstacle((float) 0.2 * width, (float) 0.7 * height, 100, 100)
+                };
+            case 2: // mid + bottom
+                return new Obstacle[]{
+                        new Obstacle((float) 0.2 * width, (float) 0.7 * height, 100, 100),
+                        new Obstacle((float) 0.5 * width, (float) 0.45 * height, 100, 100)
+                };
+        }
+        return null;
     }
 }
