c205789b95019c8e9a9d2af4d2cbde3e6217e730
deaddog <mikkelslsl@gmail.com>
2014-04-04 13:04:37 +0200

    Added touch events argument in Overlay.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/Overlay.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/Overlay.java
@@ -1,14 +1,15 @@
 package dk.aau.cs.giraf.cars.game.Overlay;
 
+import java.util.ArrayList;
+
 import dk.aau.cs.giraf.cars.framework.Game;
 import dk.aau.cs.giraf.cars.framework.Graphics;
+import dk.aau.cs.giraf.cars.framework.Input;
 import dk.aau.cs.giraf.cars.game.Interfaces.Drawable;
 import dk.aau.cs.giraf.cars.game.Interfaces.GameObject;
 import dk.aau.cs.giraf.cars.game.Interfaces.Updatable;
 
-import java.util.ArrayList;
-
-public abstract class Overlay {
+public abstract class Overlay implements GameObject {
 
     ArrayList<Drawable> drawables;
     ArrayList<Updatable> updatables;
@@ -20,33 +21,31 @@ public abstract class Overlay {
         updatables = new ArrayList<Updatable>();
     }
 
-    protected void Add(Drawable d)
-    {
-            drawables.add(d);
+    protected void Add(Drawable d) {
+        drawables.add(d);
     }
 
-    protected void Add(Updatable u)
-    {
+    protected void Add(Updatable u) {
         updatables.add(u);
     }
 
-    protected void Add(GameObject g)
-    {
+    protected void Add(GameObject g) {
         drawables.add(g);
         updatables.add(g);
     }
 
-
+    @Override
     public void Draw(Graphics graphics, float deltaTime) {
         for (Drawable i : drawables) {
-            i.Draw(graphics,deltaTime);
+            i.Draw(graphics, deltaTime);
         }
 
     }
 
-    public void Update(float deltaTime){
+    @Override
+    public void Update(Input.TouchEvent[] touchEvents, float deltaTime) {
         for (Updatable i : updatables) {
-            i.Update(deltaTime);
+            i.Update(touchEvents, deltaTime);
         }
 
     }
