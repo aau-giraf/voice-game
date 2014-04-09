c1ce88b7060db63766197dc9a8c00e755973437d
stufkan <stufkan@gmail.com>
2014-04-09 15:55:38 +0200

    Redoing the CarControls to return where the car needs to go, instead of the direction

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/Controller/VolumeCarControl.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Controller/VolumeCarControl.java
@@ -4,6 +4,7 @@ import android.media.MediaRecorder;
 
 import java.io.IOException;
 
+import android.util.Log;
 import dk.aau.cs.giraf.cars.framework.Input;
 import dk.aau.cs.giraf.cars.game.Car;
 import dk.aau.cs.giraf.cars.game.Interfaces.CarControl;
@@ -14,14 +15,29 @@ public class VolumeCarControl implements CarControl {
     private final float lower = 500f;
     private final float upper = 5000f;
 
+    public void setMinAmplitude(float minAmplitude) {
+        this.minAmplitude = minAmplitude;
+    }
+
+    public void setNormalAmplitude(float normalAmplitude) {
+        this.normalAmplitude = normalAmplitude;
+    }
+
+    public void setMaxAmplitude(float maxAmplitude) {
+        this.maxAmplitude = maxAmplitude;
+    }
+
     private float minAmplitude;
     private float normalAmplitude;
     private float maxAmplitude;
+    private float height;
 
-    public VolumeCarControl(float minAmplitude, float normalAmplitude, float maxAmplitude) {
+    public VolumeCarControl(float minAmplitude, float normalAmplitude, float maxAmplitude, int height) {
         this.minAmplitude = minAmplitude;
         this.normalAmplitude = normalAmplitude;
         this.maxAmplitude = maxAmplitude;
+        this.height = height;
+
         mediaRecorder = new MediaRecorder();
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
@@ -36,21 +52,25 @@ public class VolumeCarControl implements CarControl {
     }
 
     @Override
-    public float getMove(Input.TouchEvent[] touchEvents, Car car) {
+    public float getMove(Input.TouchEvent[] touchEvents) {
 
         float volume = (float)mediaRecorder.getMaxAmplitude();
-
+        Log.d("vol",volume+"");
         if (volume < minAmplitude)
-            return 1;
+            return 0;
 
         if (volume > maxAmplitude)
-            volume = maxAmplitude;
+            return 1;
 
-        volume -= minAmplitude;
-        volume /= (maxAmplitude - minAmplitude) / 2f;
-        volume -= 1;
+        float range = maxAmplitude-minAmplitude;
+        float multiplier = height/ range;
 
-        return -volume; //Inverted controls
+        return (volume - minAmplitude)*multiplier;
+    }
+
+    public float getAmplitude()
+    {
+        return mediaRecorder.getMaxAmplitude();
     }
 
     @Override
