64a9b0126d32e007d772801fcafd56b2e443f110
deaddog <mikkelslsl@gmail.com>
2014-04-09 15:35:16 +0200

    Added a SettingsScreen to the SpeedFragment.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/SpeedFragment.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/SpeedFragment.java
@@ -1,10 +1,49 @@
 package dk.aau.cs.giraf.cars.game;
 
+import dk.aau.cs.giraf.cars.framework.Game;
+import dk.aau.cs.giraf.cars.framework.Graphics;
+import dk.aau.cs.giraf.cars.framework.Input;
 import dk.aau.cs.giraf.cars.framework.Screen;
 
 public class SpeedFragment extends CarsFragment {
     @Override
     public Screen getFirstScreen() {
-        return null;
+        return new Screen(this, 10);
+    }
+
+    private class Screen extends SettingsScreen {
+        public Screen(Game game, int grassSize) {
+            super(game, grassSize);
+        }
+
+        @Override
+        public void paint(Graphics graphics, float deltaTime) {
+            super.paint(graphics, deltaTime);
+        }
+
+        @Override
+        public void update(Input.TouchEvent[] touchEvents, float deltaTime) {
+
+        }
+
+        @Override
+        public void pause() {
+
+        }
+
+        @Override
+        public void resume() {
+
+        }
+
+        @Override
+        public void dispose() {
+
+        }
+
+        @Override
+        public void backButton() {
+
+        }
     }
 }
