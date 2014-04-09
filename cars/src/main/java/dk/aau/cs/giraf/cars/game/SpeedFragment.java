b35f17d9fe9c0065ce261f6375705bfa199e65db
deaddog <mikkelslsl@gmail.com>
2014-04-09 15:59:24 +0200

    Speed screen draw the current speed in the bottom right corner.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/SpeedFragment.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/SpeedFragment.java
@@ -1,24 +1,41 @@
 package dk.aau.cs.giraf.cars.game;
 
+import android.graphics.Color;
+import android.graphics.Paint;
+
 import dk.aau.cs.giraf.cars.framework.Game;
 import dk.aau.cs.giraf.cars.framework.Graphics;
 import dk.aau.cs.giraf.cars.framework.Input;
-import dk.aau.cs.giraf.cars.framework.Screen;
 
 public class SpeedFragment extends CarsFragment {
     @Override
     public Screen getFirstScreen() {
-        return new Screen(this, 10);
+        Screen screen = new Screen(this, 10);
+        screen.speed = 100;
+
+        return screen;
     }
 
     private class Screen extends SettingsScreen {
+        float speed;
+        private Paint paint;
+
         public Screen(Game game, int grassSize) {
             super(game, grassSize);
+
+            setCarYToCenter();
+
+            paint = new Paint();
+            paint.setTextSize(100);
+            paint.setTextAlign(Paint.Align.RIGHT);
+            paint.setAntiAlias(true);
+            paint.setColor(Color.WHITE);
         }
 
         @Override
         public void paint(Graphics graphics, float deltaTime) {
             super.paint(graphics, deltaTime);
+            graphics.drawString(String.valueOf(speed), graphics.getWidth(), graphics.getHeight(), paint);
         }
 
         @Override
