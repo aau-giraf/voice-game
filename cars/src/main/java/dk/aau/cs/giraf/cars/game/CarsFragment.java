deeff39743a7075640fa440ef81c584c4ea668d2
deaddog <mikkelslsl@gmail.com>
2014-04-09 13:19:53 +0200

    Created a CarsFragment that ensures that assets are loading before initializing screens.

---
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/CarsFragment.java
@@ -0,0 +1,53 @@
+package dk.aau.cs.giraf.cars.game;
+
+import dk.aau.cs.giraf.cars.framework.GameFragment;
+import dk.aau.cs.giraf.cars.framework.Graphics;
+import dk.aau.cs.giraf.cars.framework.Input;
+import dk.aau.cs.giraf.cars.framework.Screen;
+
+public abstract class CarsFragment extends GameFragment {
+
+    public abstract Screen getFirstScreen();
+
+    @Override
+    public final Screen getInitScreen() {
+        return new LoadingScreen(this);
+    }
+
+    private class LoadingScreen extends Screen {
+        public LoadingScreen(CarsFragment game) {
+            super(game);
+        }
+
+        @Override
+        public void update(Input.TouchEvent[] touchEvents, float deltaTime) {
+
+        }
+
+        @Override
+        public void paint(Graphics graphics, float deltaTime) {
+            Assets.LoadAssets(graphics);
+            game.setScreen(((CarsFragment)game).getFirstScreen());
+        }
+
+        @Override
+        public void pause() {
+
+        }
+
+        @Override
+        public void resume() {
+
+        }
+
+        @Override
+        public void dispose() {
+
+        }
+
+        @Override
+        public void backButton() {
+
+        }
+    }
+}
