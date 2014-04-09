3cd43d206e49c873377fd2cee5a918548eae0a0b
stufkan <stufkan@gmail.com>
2014-04-09 15:53:56 +0200

    Adjustment to the new CarScreen superclass and changes to the calibration logic (buttons)

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/CalibrationFragment.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/CalibrationFragment.java
@@ -1,30 +1,14 @@
 package dk.aau.cs.giraf.cars.game;
 
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.os.Bundle;
-import android.os.Messenger;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import dk.aau.cs.giraf.cars.framework.GameFragment;
 import dk.aau.cs.giraf.cars.framework.Screen;
-import dk.aau.cs.giraf.cars.game.CalibrationScreen;
 
-import java.util.concurrent.CopyOnWriteArrayList;
 
-/**
- * Created by Stefan on 08-04-2014.
- */
-public class CalibrationFragment extends GameFragment{
+public class CalibrationFragment extends CarsFragment{
 
     public CalibrationFragment(){super();}
 
     @Override
-    public Screen getInitScreen() {
-
+    public Screen getFirstScreen() {
         return new CalibrationScreen(this);
     }
-
-
 }
--- cars/src/main/java/dk/aau/cs/giraf/cars/game/CalibrationScreen.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/CalibrationScreen.java
@@ -1,30 +1,65 @@
 package dk.aau.cs.giraf.cars.game;
 
+import android.graphics.Color;
+import android.text.method.Touch;
 import dk.aau.cs.giraf.cars.framework.*;
+import dk.aau.cs.giraf.cars.game.Controller.TouchCarControl;
+import dk.aau.cs.giraf.cars.game.Controller.VolumeCarControl;
+import dk.aau.cs.giraf.cars.game.Overlay.OverlayButton;
 
 /**
  * Created by Stefan on 08-04-2014.
  */
-public class CalibrationScreen extends Screen {
-    private Car car;
+public class CalibrationScreen extends SettingsScreen {
+
+    private VolumeCarControl control;
+    private final int pixelsPerSecond =200;
+    private OverlayButton loud;
+    private OverlayButton speak;
+    private OverlayButton silence;
 
     public CalibrationScreen(GameFragment game)
     {
         super(game);
-        this.car = new Car(0, 0, 200, 99);
-        this.car.x = -car.width;
-        this.car.y = 250 - car.height/2;//(game.getHeight() - car.height) / 2f;
+
+        control = new VolumeCarControl(0,500,5000, game.getHeight());
+        setCarXToCenter();
+        setCarYToCenter();
+
+        loud = new OverlayButton(20,100, Color.BLUE,Color.YELLOW,"HØJ");
+        speak = new OverlayButton(20,200, Color.BLUE,Color.YELLOW,"TALE");
+        silence = new OverlayButton(20,300, Color.BLUE,Color.YELLOW,"STILLE");
     }
 
     @Override
-    public void update(Input.TouchEvent[] touchEvents, float deltaTime) {
-        car.x+=1;
+    public void update(Input.TouchEvent[] touchEvents, float deltaTime)
+    {
+        loud.Update(touchEvents,deltaTime);
+        if(loud.IsButtonPressed(touchEvents))
+            control.setMaxAmplitude(control.getAmplitude());
+
+        speak.Update(touchEvents,deltaTime);
+        if(speak.IsButtonPressed(touchEvents))
+            control.setNormalAmplitude(control.getAmplitude());
+
+        silence.Update(touchEvents,deltaTime);
+        if(silence.IsButtonPressed(touchEvents))
+            control.setMinAmplitude(control.getAmplitude());
+
+        float move = control.getMove(touchEvents);
+        move *=pixelsPerSecond*(deltaTime/1000);
+        setCarY(getCarY()+move);
+
     }
 
     @Override
     public void paint(Graphics graphics, float deltaTime) {
-        graphics.drawARGB(255,0,0,0);
-        car.Draw(graphics,deltaTime);
+        super.paint(graphics,deltaTime);
+
+        loud.Draw(graphics,deltaTime);
+        speak.Draw(graphics,deltaTime);
+        silence.Draw(graphics,deltaTime);
+
     }
 
     @Override
@@ -39,7 +74,7 @@ public class CalibrationScreen extends Screen {
 
     @Override
     public void dispose() {
-
+        control.Stop();
     }
 
     @Override
