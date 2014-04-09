11695130805cc46af3e4edae54fe2ef1ad179aba
deaddog <mikkelslsl@gmail.com>
2014-04-09 15:30:36 +0200

    Created a fragment for setting game speed.

---
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/SpeedFragment.java
@@ -0,0 +1,10 @@
+package dk.aau.cs.giraf.cars.game;
+
+import dk.aau.cs.giraf.cars.framework.Screen;
+
+public class SpeedFragment extends CarsFragment {
+    @Override
+    public Screen getFirstScreen() {
+        return null;
+    }
+}
