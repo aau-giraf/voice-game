c02895228d3d4adb79b2613cf32735205df5173d
deaddog <mikkelslsl@gmail.com>
2014-04-10 13:27:26 +0200

    Created a CarsActivity similar to the CarsFragment.

---
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/CarsGames/CarsActivity.java
@@ -0,0 +1,54 @@
+package dk.aau.cs.giraf.cars.game.CarsGames;
+
+import dk.aau.cs.giraf.cars.framework.GameActivity;
+import dk.aau.cs.giraf.cars.framework.Graphics;
+import dk.aau.cs.giraf.cars.framework.Input;
+import dk.aau.cs.giraf.cars.framework.Screen;
+import dk.aau.cs.giraf.cars.game.Assets;
+
+public abstract class CarsActivity extends GameActivity {
+    public abstract Screen getFirstScreen();
+
+    @Override
+    public final Screen getInitScreen() {
+        return new LoadingScreen(this);
+    }
+
+    private class LoadingScreen extends Screen {
+        public LoadingScreen(CarsActivity game) {
+            super(game);
+        }
+
+        @Override
+        public void update(Input.TouchEvent[] touchEvents, float deltaTime) {
+
+        }
+
+        @Override
+        public void paint(Graphics graphics, float deltaTime) {
+            Assets.LoadAssets(graphics);
+            game.setScreen(((CarsActivity)game).getFirstScreen());
+        }
+
+        @Override
+        public void pause() {
+
+        }
+
+        @Override
+        public void resume() {
+
+        }
+
+        @Override
+        public void dispose() {
+
+        }
+
+        @Override
+        public void backButton() {
+
+        }
+    }
+
+}
