80230ba6e8773680d1de52e99deffe70170b41f2
deaddog <mikkelslsl@gmail.com>
2014-04-10 13:24:48 +0200

    Moved CarsFragment to a new directory/package 'CarsGames'.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/CalibrationFragment.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/CalibrationFragment.java
@@ -1,9 +1,10 @@
 package dk.aau.cs.giraf.cars.game;
 
 import dk.aau.cs.giraf.cars.framework.Screen;
+import dk.aau.cs.giraf.cars.game.CarsGames.CarsFragment;
 
 
-public class CalibrationFragment extends CarsFragment{
+public class CalibrationFragment extends CarsFragment {
 
     public CalibrationFragment(){super();}
 
