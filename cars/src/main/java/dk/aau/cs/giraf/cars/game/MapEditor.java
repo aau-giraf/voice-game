ac63137df9a990d99695819dc0e2b2090ae8dc32
deaddog <mikkelslsl@gmail.com>
2014-04-10 14:27:11 +0200

    The MapEditor uses touch input to add obstacles to the game.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/MapEditor.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/MapEditor.java
@@ -1,6 +1,9 @@
 package dk.aau.cs.giraf.cars.game;
 
+import java.util.ArrayList;
+
 import dk.aau.cs.giraf.cars.framework.Game;
+import dk.aau.cs.giraf.cars.framework.Graphics;
 import dk.aau.cs.giraf.cars.framework.Input;
 import dk.aau.cs.giraf.cars.framework.Screen;
 import dk.aau.cs.giraf.cars.game.CarsGames.CarsActivity;
@@ -8,18 +11,35 @@ import dk.aau.cs.giraf.cars.game.CarsGames.CarsActivity;
 public class MapEditor extends CarsActivity {
     @Override
     public Screen getFirstScreen() {
-        return null;
+        return new MapScreen(this);
     }
 
     private class MapScreen extends SettingsScreen {
+        private final int OBSTACLE_SIZE = 100;
+
+        private ArrayList<Obstacle> obstacles;
+
         public MapScreen(Game game) {
             super(game);
             setCarX(-getCarWidth());
+
+            obstacles = new ArrayList<Obstacle>();
+        }
+
+        @Override
+        public void paint(Graphics graphics, float deltaTime) {
+            super.paint(graphics, deltaTime);
+            for(Obstacle o : obstacles)
+                o.Draw(graphics, deltaTime);
         }
 
         @Override
         public void update(Input.TouchEvent[] touchEvents, float deltaTime) {
-
+            for(Input.TouchEvent e : touchEvents){
+                if(e.type == Input.TouchEvent.TOUCH_UP){
+                    obstacles.add(new Obstacle(e.x - OBSTACLE_SIZE / 2, e.y - OBSTACLE_SIZE / 2, OBSTACLE_SIZE, OBSTACLE_SIZE));
+                }
+            }
         }
 
         @Override
