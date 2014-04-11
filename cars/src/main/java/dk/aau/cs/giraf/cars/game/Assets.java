766903f1b8b93a16b5116c6bd8d2e67fe2aaa076
deaddog <mikkelslsl@gmail.com>
2014-04-11 11:24:31 +0200

    Replaced the obstacle image.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/Assets.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Assets.java
@@ -14,7 +14,7 @@ public class Assets {
         tarmac = graphics.newImage("tarmac.jpg", Graphics.ImageFormat.RGB565);
         border = graphics.newImage("border.png", Graphics.ImageFormat.ARGB8888);
         car = graphics.newImage("car.png", Graphics.ImageFormat.ARGB8888);
-        obstacle = graphics.newImage("obstacle.jpg", Graphics.ImageFormat.RGB565);
+        obstacle = graphics.newImage("obstacle.png", Graphics.ImageFormat.ARGB8888);
         trophy = graphics.newImage("trophy.png", Graphics.ImageFormat.ARGB8888);
         garage = graphics.newImage("garage.png", Graphics.ImageFormat.ARGB8888);
         explosion = graphics.newImage("explosion.png", Graphics.ImageFormat.ARGB8888);
