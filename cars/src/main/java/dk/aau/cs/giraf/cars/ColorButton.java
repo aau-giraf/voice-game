a127a862ff79077814e5d05d1d993c55b3b6f6f1
mikaelec <mikaelec@gmail.com>
2014-04-28 15:26:33 +0200

    Added big color buttons for choosing car/garage colors. Also removed ok button and moved settings save logic to onDestroy.

---
+++ cars/src/main/java/dk/aau/cs/giraf/cars/ColorButton.java
@@ -0,0 +1,26 @@
+package dk.aau.cs.giraf.cars;
+
+class ColorButton extends android.widget.Button {
+    private int color;
+
+    public ColorButton(android.content.Context context) {
+        super(context);
+    }
+
+    public ColorButton(android.content.Context context, android.util.AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ColorButton(android.content.Context context, android.util.AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void SetColor(int color) {
+        this.color = color;
+        getBackground().setColorFilter(color, android.graphics.PorterDuff.Mode.MULTIPLY);
+    }
+
+    public int GetColor() {
+        return this.color;
+    }
+}
