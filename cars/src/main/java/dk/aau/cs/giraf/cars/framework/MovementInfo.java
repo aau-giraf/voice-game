06e3de43ab944f6052883d3f55bc613368ebc15c
Mikkel Sandø Larsen <milars11@student.aau.dk>
2014-04-30 11:51:39 +0200

    Created classes for implementing mFloats.

---
+++ cars/src/main/java/dk/aau/cs/giraf/cars/framework/MovementInfo.java
@@ -0,0 +1,9 @@
+package dk.aau.cs.giraf.cars.framework;
+
+/**
+ * Created by Mikkel on 30-04-2014.
+ */
+public abstract class MovementInfo {
+    public abstract float getPointEnd();
+    public abstract float getPointStart();
+}
