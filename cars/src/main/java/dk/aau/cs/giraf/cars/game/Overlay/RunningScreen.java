2fb4168cf1bc263a12b6de15d59f2fdf615a45d3
Ham den Seje <mr.thalmann@gmail.com>
2014-05-02 09:39:07 +0200

    added new screen which will handle when the game runs normally

---
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/RunningScreen.java
@@ -0,0 +1,12 @@
+package dk.aau.cs.giraf.cars.game.Overlay;
+
+import dk.aau.cs.giraf.cars.framework.GameActivity;
+import dk.aau.cs.giraf.cars.game.GameScreen;
+import dk.aau.cs.giraf.cars.game.GameSettings;
+import dk.aau.cs.giraf.cars.game.ObstacleGenerator;
+
+public class RunningScreen extends GameScreen {
+    public RunningScreen(GameActivity game, ObstacleGenerator obstacleGenerator, GameSettings gs) {
+        super(game, obstacleGenerator, gs);
+    }
+}
