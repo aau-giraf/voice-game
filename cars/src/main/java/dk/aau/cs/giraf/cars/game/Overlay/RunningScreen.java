ac78261cab7f58b5e47ad4f8be27dcaacb34ef64
Ham den Seje <mr.thalmann@gmail.com>
2014-05-05 13:54:01 +0200

    moved a lot from running screen to gamescreen, because it is used in the other screens aswell

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/RunningScreen.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/RunningScreen.java
@@ -28,17 +28,14 @@ import dk.aau.cs.giraf.cars.game.ObstacleGenerator;
 
 public class RunningScreen extends GameScreen {
     private final boolean debug = false;
-    private ArrayList<Garage> garages;
-    private int amountOfGarages = 3;
-    private final float garageSize = 250;
-    private LinkedList<Integer> colors;
+
+
 
     private float speed; //Pixels per second
-    private final float animationZoneSize = 100;
-    private float animationZoneX;
-    private ArrayList<Obstacle> obstacles;
-    private Rect pauseButtonRec = new Rect(20, 20, 100, 100);
-    private Rect pauseButtonImageRec = new Rect(0, 0, Assets.GetPlayButton().getWidth(), Assets.GetPlayButton().getHeight());
+
+
+
+
 
     public RunningScreen(GameActivity game, ObstacleGenerator obstacleGenerator, GameSettings gs) {
         super(game, obstacleGenerator, gs);
@@ -48,27 +45,11 @@ public class RunningScreen extends GameScreen {
 
         this.speed = gs.GetSpeed() * (Car.MAX_PIXELSPERSECOND / Car.MAX_SCALE);
 
-        colors = (LinkedList<Integer>) gs.GetColors().clone();
-        Collections.shuffle(colors);
 
-        this.garages = new ArrayList<Garage>();
-        float garageSpace = (game.getHeight() - 2 * grassSize - 3 * garageSize) / 4f;
-        for (int i = 0; i < amountOfGarages; i++) {
-            Garage g = new Garage(game.getWidth() - garageSize, grassSize + (i + 1) * garageSpace + i * garageSize + garageSize / 4, garageSize, garageSize / 2);
-            Log.d("Settings", colors.toString());
-            g.setColor(colors.get(i));
-            garages.add(g);
-        }
 
-        Collections.shuffle(colors);
-        car.setColor(colors.removeFirst());
 
-        this.animationZoneX = garages.get(0).x - animationZoneSize;
 
-        this.obstacles = new ArrayList<Obstacle>();
 
-        for (Obstacle o : obstacleGenerator.CreateObstacles(game.getWidth(), game.getHeight()))
-            this.obstacles.add(o);
     }
 
     @Override
@@ -100,28 +81,7 @@ public class RunningScreen extends GameScreen {
         if (car.y > game.getHeight() - car.height - grassSize)
             car.y = game.getHeight() - car.height - grassSize;
 
-        for (int i = 0; i < obstacles.size(); i++) {
-            obstacles.get(i).Update(touchEvents, deltaTime);
-            if (obstacles.get(i).CollidesWith(car)) {
-                crashedOverlay.setLastCrash(obstacles.get(i).GetCollisionCenter(car));
-                state = GameState.Crashed;
-                return;
-            }
-        }
 
-        for (Garage garage : garages) {
-            garage.Update(touchEvents, deltaTime);
-            if (garage.CollidesWith(car) && garage.color != car.color) {
-                crashedOverlay.setLastCrash(garage.GetCollisionCenter(car));
-                state = GameState.Crashed;
-                return;
-            } else {
-                if (car.getColor() == garage.getColor() && car.GetBounds().left > garage.GetBounds().left) {
-                    garage.Close();
-                    state = GameState.Closing;
-                }
-            }
-        }
     }
 
     @Override
@@ -148,67 +108,10 @@ public class RunningScreen extends GameScreen {
 
         car.Draw(graphics, deltaTime);
 
-        for (Obstacle obstacle : obstacles)
-            obstacle.Draw(graphics, deltaTime);
 
-        for (Garage garage : garages)
-            garage.Draw(graphics, deltaTime);
-
-        graphics.drawScaledImage(Assets.GetPauseButton(), pauseButtonRec, pauseButtonImageRec);
     }
 
 
-    private boolean allGaragesClosed() {
-        int counter = 0;
-        for (Garage garage : garages) {
-            if (garage.getIsClosed())
-                counter++;
-        }
-        if (amountOfGarages == counter)
-            return true;
-        return false;
-    }
 
-    private float getGarageTargetY() {
-        float minDist = Float.MAX_VALUE;
-        Garage garage = null;
-
-        for (Garage g : garages) {
-            float dist = Math.abs(car.y - g.y);
-            if (dist < minDist) {
-                minDist = dist;
-                garage = g;
-            }
-        }
-        return garage.y + garage.height / 2f;
-    }
-
-    private void updateClosing(Input.TouchEvent[] touchEvents, float deltaTime) {
-        for (Garage g : garages) {
-            Log.d("garage", String.format("Car: %S Garage: %S CarLeft: %S GarageLeft: %S", car.getColor(), g.getColor(), car.GetBounds().left, g.GetBounds().left));
-            g.Update(touchEvents, deltaTime);
-
-            if (car.getColor() == g.getColor() && car.GetBounds().left > g.GetBounds().left && g.getIsClosed()) {
-                resetRound();
-                Log.d("garage", "reseT?");
-                state = GameState.Running;
-            }
-        }
-    }
-
-    public void resetRound() {
-        boolean newColor = ResetCarColor();
-
-        if (newColor)
-            car.ResetCar(game.getHeight(), grassSize, verticalMover);
-    }
-
-    private boolean ResetCarColor() {
-        if (colors.size() > 0) //This shouldn't be necessary, but game doesn't quite stop even though all 3 garages are closed
-        {
-            car.setColor(colors.removeFirst());
-            return true;
-        } else return false;
-    }
 
 }
