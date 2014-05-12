f8b8479b53eb2384aa336fdfce26fa189600a404
Mikkel Sandø Larsen <milars11@student.aau.dk>
2014-05-12 15:59:36 +0200

    Updated the constructor for the RunningScreen.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/RunningScreen.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/RunningScreen.java
@@ -2,10 +2,10 @@ package dk.aau.cs.giraf.cars.game.Overlay;
 
 import android.graphics.Rect;
 
-import dk.aau.cs.giraf.cars.framework.GameActivity;
 import dk.aau.cs.giraf.cars.framework.Graphics;
 import dk.aau.cs.giraf.cars.framework.Input;
 import dk.aau.cs.giraf.cars.game.Assets;
+import dk.aau.cs.giraf.cars.game.CarGame;
 import dk.aau.cs.giraf.cars.game.GameScreen;
 import dk.aau.cs.giraf.cars.game.GameSettings;
 import dk.aau.cs.giraf.cars.game.ObstacleGenerator;
@@ -14,7 +14,7 @@ public class RunningScreen extends GameScreen {
     private Rect pauseButtonRec = new Rect(20, 20, 100, 100);
     private Rect pauseButtonImageRec = new Rect(0, 0, Assets.GetPlayButton().getWidth(), Assets.GetPlayButton().getHeight());
 
-    public RunningScreen(GameActivity game, ObstacleGenerator obstacleGenerator, GameSettings gs) {
+    public RunningScreen(CarGame game, ObstacleGenerator obstacleGenerator, GameSettings gs) {
         super(game, obstacleGenerator, gs);
     }
 
@@ -27,7 +27,6 @@ public class RunningScreen extends GameScreen {
     public void paint(Graphics graphics, float deltaTime) {
         super.paint(graphics, deltaTime);
 
-        if (isCarInAnimaitonZone())
-            graphics.drawScaledImage(Assets.GetPauseButton(), pauseButtonRec, pauseButtonImageRec);
+        graphics.drawScaledImage(Assets.GetPauseButton(), pauseButtonRec, pauseButtonImageRec);
     }
 }
