2dadc03acd20dd92d9c0f1b333d6b47a456be7f7
Mikkel Sandø Larsen <milars11@student.aau.dk>
2014-05-12 21:46:52 +0200

    Implemented the RunningScreen, except for the actual move of the car.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/RunningScreen.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/RunningScreen.java
@@ -5,22 +5,33 @@ import android.graphics.Rect;
 import dk.aau.cs.giraf.cars.framework.Graphics;
 import dk.aau.cs.giraf.cars.framework.Input;
 import dk.aau.cs.giraf.cars.game.Assets;
+import dk.aau.cs.giraf.cars.game.Car;
 import dk.aau.cs.giraf.cars.game.CarGame;
 import dk.aau.cs.giraf.cars.game.GameScreen;
 import dk.aau.cs.giraf.cars.game.GameSettings;
+import dk.aau.cs.giraf.cars.game.Obstacle;
+import dk.aau.cs.giraf.cars.game.ObstacleCollection;
 import dk.aau.cs.giraf.cars.game.ObstacleGenerator;
 
 public class RunningScreen extends GameScreen {
     private Rect pauseButtonRec = new Rect(20, 20, 100, 100);
     private Rect pauseButtonImageRec = new Rect(0, 0, Assets.GetPlayButton().getWidth(), Assets.GetPlayButton().getHeight());
 
-    public RunningScreen(CarGame game, ObstacleGenerator obstacleGenerator, GameSettings gs) {
-        super(game, obstacleGenerator, gs);
+    public RunningScreen(CarGame game, Car car, ObstacleCollection obstacles) {
+        super(game, car, obstacles);
     }
 
     @Override
     public void update(Input.TouchEvent[] touchEvents, float deltaTime) {
         super.update(touchEvents, deltaTime);
+
+        Obstacle obstacle = findCollision();
+        if (obstacle != null)
+            showCrashScreen(obstacle);
+
+        for (Input.TouchEvent e : touchEvents)
+            if (e.type == Input.TouchEvent.TOUCH_DOWN && e.inBounds(pauseButtonRec))
+                showPauseScreen();
     }
 
     @Override
