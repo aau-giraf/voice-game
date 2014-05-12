658589ed2cff586f00abd71f71a303b914d09356
Mikkel Sandø Larsen <milars11@student.aau.dk>
2014-05-12 21:18:19 +0200

    Created the shell for a collection that handles obstacles in the game.

---
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/ObstacleCollection.java
@@ -0,0 +1,18 @@
+package dk.aau.cs.giraf.cars.game;
+
+import dk.aau.cs.giraf.cars.framework.Graphics;
+import dk.aau.cs.giraf.cars.framework.Input;
+import dk.aau.cs.giraf.cars.game.Interfaces.GameObject;
+
+public class ObstacleCollection implements GameObject {
+
+    @Override
+    public void Draw(Graphics graphics, float deltaTime) {
+        
+    }
+
+    @Override
+    public void Update(Input.TouchEvent[] touchEvents, float deltaTime) {
+
+    }
+}
