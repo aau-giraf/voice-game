74346db53ee2a0c837fc2532a53b4af14f91c481
Ham den Seje <mr.thalmann@gmail.com>
2014-05-12 09:34:58 +0200

    added overlays to cargame, so cargame manages which overlay to show

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/CarGame.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/CarGame.java
@@ -7,14 +7,32 @@ import android.os.Bundle;
 import dk.aau.cs.giraf.cars.framework.GameActivity;
 import dk.aau.cs.giraf.cars.framework.Screen;
 import dk.aau.cs.giraf.cars.game.CarsGames.CarsActivity;
+import dk.aau.cs.giraf.cars.game.Overlay.CrashOverlay;
+import dk.aau.cs.giraf.cars.game.Overlay.PauseOverlay;
+import dk.aau.cs.giraf.cars.game.Overlay.RunningScreen;
 import dk.aau.cs.giraf.cars.game.Overlay.StartOverlay;
+import dk.aau.cs.giraf.cars.game.Overlay.WinningOverlay;
 
 public class CarGame extends CarsActivity {
     GameSettings gamesettings;
+    StartOverlay startOverlay;
+    CrashOverlay crashOverlay;
+    PauseOverlay pauseOverlay;
+    WinningOverlay winningOverlay;
+    RunningScreen runningScreen;
+
+    public CarGame() {
+        super();
+        startOverlay = new StartOverlay(this, new PreferencesObstacles(this), gamesettings);
+        crashOverlay = new CrashOverlay(this, new PreferencesObstacles(this), gamesettings);
+        pauseOverlay = new PauseOverlay(this, new PreferencesObstacles(this), gamesettings);
+        winningOverlay = new WinningOverlay(this, new PreferencesObstacles(this), gamesettings);
+        runningScreen = new RunningScreen(this, new PreferencesObstacles(this), gamesettings);
+    }
 
     @Override
     public Screen getFirstScreen() {
-        return new StartOverlay(this, new PreferencesObstacles(this), gamesettings);
+        return startOverlay;
     }
 
     @Override
@@ -26,5 +44,13 @@ public class CarGame extends CarsActivity {
 
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+    }
+
+    public void ShowStartOverlay() {
+        setScreen(startOverlay);
+    }
+
+    public void ShowRunningScreen() {
+        setScreen(runningScreen);
     }
 }
