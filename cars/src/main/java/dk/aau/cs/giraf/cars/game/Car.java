9b95db406657c9aaff38ffe5e66fff8b271ea970
Mikkel Sandø Larsen <milars11@student.aau.dk>
2014-05-13 11:03:53 +0200

    Renamed the ResetCar method in Car.

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/Car.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Car.java
@@ -94,7 +94,7 @@ public class Car extends GameItem {
         super.x += pixelSpeed * (deltaTime / 1000.0f);
     }
 
-    public Car ResetCar() {
+    public Car resetCar() {
         super.x = initialX;
         super.y = initialY;
         verticalMover.setCurrentValue(initialY);
