9ebd9c3ae5da66eb47fc27cbf7d78ef306999176
Ham den Seje <mr.thalmann@gmail.com>
2014-05-15 10:27:35 +0200

    renamed ObstacleCollection to GameItemCollection

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/CrashScreen.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/CrashScreen.java
@@ -12,7 +12,7 @@ public class CrashScreen extends GameScreen {
     private Rect lastCrash;
     private Rect explosionRect;
 
-    public CrashScreen(CarGame game, Car car, ObstacleCollection obstacles) {
+    public CrashScreen(CarGame game, Car car, GameItemCollection obstacles) {
         super(game, car, obstacles);
 
         continueButton = new OverlayButton(game.getWidth() / 2, game.getHeight() / 2, game.getResources().getString(R.string.crash_button_text));
