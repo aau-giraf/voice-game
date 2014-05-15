3196dca6ddae99c2888def594c3c05ce588e1be6
Ham den Seje <mr.thalmann@gmail.com>
2014-05-15 10:27:05 +0200

    renamed ObstacleCollection to GameItemCollection

--- cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/AvoidRunningScreen.java
+++ cars/src/main/java/dk/aau/cs/giraf/cars/game/Overlay/AvoidRunningScreen.java
@@ -4,19 +4,19 @@ import dk.aau.cs.giraf.cars.framework.Input;
 import dk.aau.cs.giraf.cars.game.Assets;
 import dk.aau.cs.giraf.cars.game.Car;
 import dk.aau.cs.giraf.cars.game.CarGame;
+import dk.aau.cs.giraf.cars.game.GameItemCollection;
 import dk.aau.cs.giraf.cars.game.Interfaces.CarControl;
 import dk.aau.cs.giraf.cars.game.Obstacle;
-import dk.aau.cs.giraf.cars.game.ObstacleCollection;
 
 public class AvoidRunningScreen extends RunningScreen{
-    public AvoidRunningScreen(CarGame game, Car car, ObstacleCollection obstacles, CarControl carControl, float carSpeed) {
+    public AvoidRunningScreen(CarGame game, Car car, GameItemCollection obstacles, CarControl carControl, float carSpeed) {
         super(game, car, obstacles, carControl, carSpeed);
     }
 
     @Override
     public void update(Input.TouchEvent[] touchEvents, float deltaTime){
         super.update(touchEvents,deltaTime);
-        Obstacle obstacle = getCollisionObstacle();
+        Obstacle obstacle = getCollisionRoadItem();
         if (obstacle != null) {
             Assets.GetCrash().play(1.0f);
             showCrashScreen(obstacle);
